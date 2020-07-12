eddc22687f14cb48910069f92a42d5e8817b2a67
Zachary Yaro <zmyaro@zmyaro.com>
2020-07-12 14:01:44 -0400

    Make camera lerp to player's position instead of being fixed

--- Blocks/Assets/Scripts/FollowPlayer.cs
+++ Blocks/Assets/Scripts/FollowPlayer.cs
@@ -6,15 +6,19 @@ public class FollowPlayer : MonoBehaviour
 {
 	public Player player;
 	public Vector2 offset;
+	public float followSpeedFactor = 1;
 
 	/// <summary>
 	/// Called once per frame.
 	/// </summary>
 	void Update()
 	{
-		transform.position = new Vector3(
-			player.transform.position.x + offset.x,
-			player.transform.position.y + offset.y,
-			transform.position.z);
+		transform.position = Vector3.Lerp(
+			transform.position,
+			new Vector3(
+				player.transform.position.x + offset.x,
+				player.transform.position.y + offset.y,
+				transform.position.z),
+			Time.deltaTime * followSpeedFactor);
 	}
 }
