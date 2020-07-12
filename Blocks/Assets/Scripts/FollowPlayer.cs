2f4b7a723836e5d4886df7d5a5924652f989bef1
Zachary Yaro <zmyaro@zmyaro.com>
2020-07-12 00:21:27 -0400

    Add background and wall light prefab

---
+++ Blocks/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public Player player;
+
+	/// <summary>
+	/// Called once per frame.
+	/// </summary>
+	void Update()
+	{
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+	}
+}
